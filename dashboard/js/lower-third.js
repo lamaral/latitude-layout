7de56be5b67d934da4ec8764d9e3afe30393418a
Luiz Fernando Ribeiro Amaral <email@luiz.eng.br>
2017-10-27 18:07:24 -0200

    New images and assets

--- dashboard/js/lower-third.js
+++ dashboard/js/lower-third.js
@@ -4,6 +4,13 @@
 	window.addEventListener('WebComponentsReady', function(e) {
 		var $lowerThirdHide = $('.latitude-third-hide');
 		var $lowerThirdUpdate = $('.latitude-third-update');
+    var namesReplicant = nodecg.Replicant('assets:names');
+    var names;
+
+    namesReplicant.on('change', function(newValue, oldValue) {
+			names = newValue;
+			console.log(names[0].url);
+    });
 
     $lowerThirdHide.click(function() {
 			nodecg.sendMessage('lowerThirdHide', lowerThirdData());
--- graphics/js/lower-third.js
+++ graphics/js/lower-third.js
@@ -11,9 +11,21 @@ $(function () {
 	function update(data) {
     $('#lowerthirdtoptext').text(data.top);
 		$('#lowerthirdbottomtext').text(data.bottom);
-		$('#lowerthirdcontainer').animate({left: "0%"}, 800);
-    setInterval(function() {
-        $('#lowerthirdcontainer').animate({left: "-100%"}, 800);
-    }, 6000)
+		$('#logocontainer').animate({left: "0%"}, {duration: 800, complete: function() {
+      console.log('open lower container');
+      $('#lowerthirdcontainer').animate({left: "0%"}, {duration: 800, complete: function() {
+        console.log('interval')
+        setTimeout(function() {
+            console.log('close third container')
+            $('#lowerthirdcontainer').animate({left: "-100%"}, {duration: 800, complete: function() {
+              console.log('close logo container')
+              $('#logocontainer').animate({left: "-100%"}, 800);
+            }});
+        }, 5000);
+      }});
+    }});
+    // setInterval(function() {
+    //     $('#lowerthirdcontainer').animate({left: "-100%"}, 800);
+    // }, 5000);
 	}
 });
