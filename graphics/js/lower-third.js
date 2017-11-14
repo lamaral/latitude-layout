26f24597bf088ccefe5fce6961be0c9e1e00415d
Luiz Amaral <email@luiz.eng.br>
2017-11-14 00:45:03 -0200

    Fit Text nos Lower thirds

--- graphics/js/lower-third.js
+++ graphics/js/lower-third.js
@@ -8,14 +8,16 @@ $(function () {
 
 	function hide(data) {
     clearTimeout(this.timer);
-    $('#lowerthirdcontainer').animate({left: "-100%"}, {duration: 800, complete: function() {
-      $('#logocontainer').animate({left: "-100%"}, 800);
+    $('#lowerthirdcontainer').animate({left: "-100%"}, {duration: 1, complete: function() {
+      $('#logocontainer').animate({left: "-100%"}, 1);
     }});
 	}
 
 	function update(data) {
     $('#lowerthirdtoptext').text(data.top);
 		$('#lowerthirdbottomtext').text(data.bottom);
+    textFit($('#lowerthirdtoptext'), {minFontSize:17, maxFontSize: 90, alignVert: true});
+    textFit($('#lowerthirdbottomtext'), {minFontSize:17, maxFontSize: 90, alignVert: true});
 		$('#logocontainer').animate({left: "0%"}, {duration: 800, complete: function() {
       $('#lowerthirdcontainer').animate({left: "0%"}, {duration: 800, complete: function() {
         this.timer = setTimeout(function() {
@@ -25,8 +27,5 @@ $(function () {
         }, 5000);
       }});
     }});
-    // setInterval(function() {
-    //     $('#lowerthirdcontainer').animate({left: "-100%"}, 800);
-    // }, 5000);
 	}
 });
